ced795562d1e96692bc6dbf9aa70a17527c6955b
Atilla Saadat <atillaabc@gmail.com>
2019-03-26 17:39:48 -0400

    git fix?

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -15,10 +15,15 @@
 #define errPin 6
 #define isTransPin 7
 
-//RH_ASK transmitterBoard(2000,2,3,4,false);          //create a radiohead ASK object
-RH_ASK transmitterBoard;
 String output = "";
 String dOutput = "";
+
+RH_ASK transmitterBoard;          //create a radiohead ASK object
+int count = 0;
+char transmitBuffer[255]; // since RH_ASK.send only uses a uint8_t to denote length, the max amount of information that can be sent is 255 bytes (or a string of length 254)
+
+unsigned long send_large_data(RH_ASK *transmitter, const uint8_t * dOut, unsigned long dataLength);
+
 void setup(){
 
   pinMode(successPin, OUTPUT);
@@ -60,4 +65,43 @@ void loop(){
       delay(5000);
     }
   }
+}
+
+
+/*
+ * send_large_data
+ * Aaron Li
+ * Solution for sending more than 255 bytes at a time.
+ * Sends the data in MAX_BURST_SIZE byte chunks when possible and waits for the data to be transmitted
+ * returns the number of bytes succesfully transmitted, breaking if a burst of data did not transmit correctly
+ * 
+ * In an image of 8 bit rgb pixels you can send 21845 uncompressed full colour pixels with 65535 bytes or 1,431,655,765 uncompressed full colour pixels with ~4GB
+ * 
+*/
+
+#define MAX_BURST_SIZE 255
+
+unsigned int send_large_data(RH_ASK *transmitter, uint8_t * dOut, unsigned int dataLength){
+  //max length is 2**16-1 (65,535 | ~64KB) on ATMEGA based boards and 2**32-1 (4,294,967,295 | ~4GB) on the Due, or whatever a size_t is (returned by strlen)
+  unsigned int sent = 0;
+
+  for(unsigned int i = 0; i<dataLength; i+=MAX_BURST_SIZE){
+    uint8_t burst_length = (uint8_t)min(dataLength - i, MAX_BURST_SIZE); // we're either sending the full burst or what's left of the message
+    bool can_send = transmitter->send(dOut+i, burst_length); // start the data pointer at the offset and send up to the burst length
+
+    if(!can_send){ // "the message is too long for the underlying radio technology"
+      break;
+    }
+    transmitter->waitPacketSent(); // wait for burst to send
+
+    uint8_t sent_bytes = transmitter->txGood(); // check how many bytes were sent
+
+    if(sent_bytes != burst_length){ // if the number sent is not the amount we wanted to send
+      break; // error occured
+    }
+    else{
+      sent += sent_bytes; // add sent count to our total sent
+    }
+  }
+  return sent;
 }
